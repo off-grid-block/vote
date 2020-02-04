0eadd84f960a3477b3bc16a75e654c0a8e200366
Brian Li <brianli1117@gmail.com>
2020-02-03 23:24:59 -0500

    sdk initialization successful

--- sdk/blockchain/setup.go
+++ sdk/blockchain/setup.go
@@ -29,6 +29,51 @@ type FabricSetup struct {
 	sdk             *fabsdk.FabricSDK
 }
 
+func (setup *FabricSetup) InstallAndInstantiateCC() error {
+
+	// Create the chaincode package that will be sent to the peers
+	ccPkg, err := packager.NewCCPackage(setup.ChaincodePath, setup.ChaincodeGoPath)
+	if err != nil {
+		return errors.WithMessage(err, "failed to create chaincode package")
+	}
+	fmt.Println("ccPkg created")
+
+	// Install example cc to org peers
+	installCCReq := resmgmt.InstallCCRequest{Name: setup.ChainCodeID, Path: setup.ChaincodePath, Version: "0", Package: ccPkg}
+	_, err = setup.admin.InstallCC(installCCReq, resmgmt.WithRetry(retry.DefaultResMgmtOpts))
+	if err != nil {
+		return errors.WithMessage(err, "failed to install chaincode")
+	}
+	fmt.Println("Chaincode installed")
+
+	// Set up chaincode policy
+	ccPolicy := cauthdsl.SignedByAnyMember([]string{"org1.hf.chainhero.io"})
+
+	resp, err := setup.admin.InstantiateCC(setup.ChannelID, resmgmt.InstantiateCCRequest{Name: setup.ChainCodeID, Path: setup.ChaincodeGoPath, Version: "0", Args: [][]byte{[]byte("init")}, Policy: ccPolicy})
+	if err != nil || resp.TransactionID == "" {
+		return errors.WithMessage(err, "failed to instantiate the chaincode")
+	}
+	fmt.Println("Chaincode instantiated")
+
+	// Channel client is used to query and execute transactions
+	clientContext := setup.sdk.ChannelContext(setup.ChannelID, fabsdk.WithUser(setup.UserName))
+	setup.client, err = channel.New(clientContext)
+	if err != nil {
+		return errors.WithMessage(err, "failed to create new channel client")
+	}
+	fmt.Println("Channel client created")
+
+	// Creation of the client which will enables access to our channel events
+	setup.event, err = event.New(clientContext)
+	if err != nil {
+		return errors.WithMessage(err, "failed to create new event client")
+	}
+	fmt.Println("Event client created")
+
+	fmt.Println("Chaincode Installation & Instantiation Successful")
+	return nil
+}
+
 // Initialize reads the configuration file and sets up the client, chain and event hub
 func (setup *FabricSetup) Initialize() error {
 
